5e99c6013a3c59821eee4298dbd2220f4e230daf
pavunato <15843773+pavunato@users.noreply.github.com>
2024-05-02 14:10:22 +0700

    feat: external contracts

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -1,4 +1,6 @@
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
+import abi_market from "./abi/abi.sta_market.json"
+import abi_weapon from "./abi/starmechweapon.json"
 
 /**
  * @example
@@ -10,6 +12,17 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
  *    }
  * } as const;
  */
-const externalContracts = {} as const;
+const externalContracts = {
+    43113: {
+        STA_Market: {
+            address: "0xb06C56b0696dE9d5e418fF0386330b6Da48E27DA",
+            abi: abi_market,
+        },
+        STA_Weapon: {
+            address: "0xd6300E8Af9c78672A21362507D8f4B4611F5a793",
+            abi: abi_weapon
+        },
+    }
+} as const;
 
 export default externalContracts satisfies GenericContractsDeclaration;
